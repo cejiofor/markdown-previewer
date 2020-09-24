51e5adb4b89c97525373578427cc200825bfcb07
cejiofor <cejiofor@gmail.com>
2020-09-24 12:10:00 -0500

    Add components for User Story 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Editor from './components/Editor';
+import Previewer from './components/Previewer'
 import logo from './logo.svg';
 import './App.css';
+import ReactFCCTest from 'react-fcctest';
 
 function App() {
   return (
     <div className="App">
+    <ReactFCCTest />
       <header className="App-header">
+        
+        <Editor/>
+        <Previewer />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Previewer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button, Card, Typography} from '@material-ui/core';
+
+function Previewer() {
+    return (
+        <div>
+           <Card id="preview" >
+            </Card> 
+        </div>
+    )
+}
+
+export default Previewer
+
+
