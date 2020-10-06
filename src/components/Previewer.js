fb65cc45961cc52fcbe129b30928c996f0c8a00a
cejiofor <cejiofor@gmail.com>
2020-10-06 17:55:39 -0500

    Add and implement sanititzeHtml package to santizie HTML output

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import {Button, Card, Typography} from '@material-ui/core';
 import marked from "marked";
+import sanitizeHtml from 'sanitize-html';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 function Previewer({text, onChange}) {
-    const markedText = marked(text, {sanitize: true});
+    const markedText = marked(text, {gfm: true, breaks:true});
+    // const cleanMarkedText = sanitizeHTML(markedText);
     // const renderer = new marked.Renderer();
     // var rawMarkup = marked('This is _Markdown_.', {sanitize: true});
 
     return (
         <div>
-           <Card id="preview" onChange={onChange}>
-               <Typography dangerouslySetInnerHTML={{ __html: marked(markedText) }}>
-               </Typography>
-                
+            <Card onChange={onChange}>
+               <div id="preview" dangerouslySetInnerHTML={{ __html:  sanitizeHtml(marked(markedText), {
+                   allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
+               }) }}>
+            </div>
             </Card> 
+            
         </div>
     )
 }
